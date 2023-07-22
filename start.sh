d01e6e9c348a1e9d205743eee5325fefdfe25582
XiaoKunGe1203 <119796222+XiaoKunGe1203@users.noreply.github.com>
2023-07-22 18:38:19 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,14 +4,44 @@ echo '___   |/  /_  ____/___________ '
 echo '__  /|_/ /_  /    ___/ /___/ /_'
 echo '_  /  / / / /___  /_  __/_  __/'
 echo '/_/  /_/  \____/   /_/   /_/   #bulid2307221810'
-echo '输入tools进入工具箱
-输入start启动服务器
-[container@mcpp ~]# >'
+echo '
+输入bulid进入重新部署
+输入tools进入工具箱
+输入run启动服务器
+[container@mcpp ~]# '
 
 read -p "输入: " SOFTWARE </dev/tty
 
 case $SOFTWARE in
-
+bulid)
+clear
+rm -rf /home/container/tmp
+clear
+echo '______  ___________            '
+echo '___   |/  /_  ____/___________ '
+echo '__  /|_/ /_  /    ___/ /___/ /_'
+echo '_  /  / / / /___  /_  __/_  __/'
+echo '/_/  /_/  \____/   /_/   /_/  
+开始进行部署工程'
+mkdir /home/container/tmp
+cd /home/container/tmp
+wget http://10.0.2.22/bedrock-server-1.20.12.01.zip
+unzip bedrock-server-1.20.12.01.zip
+cp -a allowlist.json ..
+cp -a permissions.json ..
+cp -a server.properties ..
+cd ..
+rm -rf tmp
+clear
+echo '______  ___________            '
+echo '___   |/  /_  ____/___________ '
+echo '__  /|_/ /_  /    ___/ /___/ /_'
+echo '_  /  / / / /___  /_  __/_  __/'
+echo '/_/  /_/  \____/   /_/   /_/   '
+echo '==============================='
+echo '部署结束，请重新启动！'
+echo '==============================='
+;;
 tools)
 clear
 echo '______  ___________            '
@@ -22,7 +52,7 @@ echo '/_/  /_/  \____/   /_/   /_/
 具体还没写呢'
 ;;
 
-start)
+run)
 clear
 rm -rf /home/container/tmp
 echo '______  ___________            '
@@ -37,11 +67,16 @@ mkdir /home/container/tmp
 cd /home/container/tmp
 wget http://10.0.2.22/bedrock-server-1.20.12.01.zip
 unzip bedrock-server-1.20.12.01.zip
+rm -rf bedrock-server-1.20.12.01.zip
 cp -l -a behavior_packs ..
 cp -l -a resource_packs ..
 cp -l -a definitions ..
 cp -l -a config ..
-cd ..
+cp -l -a bedrock_server_symbols.debug ..
+
+
+
+cd /home/container/
 clear
 echo '==============================='
 echo '服务器启动'
