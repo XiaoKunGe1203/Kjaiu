5fd95982b8777458719d65ac8a8375fba39d94bc
XiaoKunGe1203 <119796222+XiaoKunGe1203@users.noreply.github.com>
2023-07-22 15:09:50 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,28 +7,9 @@ echo '/_/  /_/  \____/   /_/   /_/   '
                                
 echo '==============================='
 echo '正在下载服务端'
-echo '==============================='
-cd /tmp
-echo '====================='
-echo 'shell'
-echo '====================='
-
-read -p "输入: " JAVA_SOFTWARE </dev/tty
-$JAVA_SOFTWARE
-
-read -p "输入: " JAVA_SOFTWAR </dev/tty
-
-case $JAVA_SOFTWAR in
-y)
-sh /tmp/open.sh
-;;
-
-esac
-
-
-
-
-#wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.20.12.01.zip
+echo '===============================
+cd /home/container'
+wget https://minecraft.azureedge.net/bin-linux/bedrock-server-1.20.12.01.zip
 #ls
 #unzip bedrock-server-1.20.12.01.zip
 #rm -rf bedrock-server-1.20.12.01.zip
