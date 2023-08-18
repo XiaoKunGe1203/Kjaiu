5544a5678e8f3177fe6f92753a2fe87779880b1a
XiaoKunGe1203 <119796222+XiaoKunGe1203@users.noreply.github.com>
2023-08-18 17:57:59 +0800

    Create start.sh

---
+++ efxnbbs/start.sh
@@ -0,0 +1,95 @@
+bnmb(){
+    echo '#bulid2307221930'
+}
+logo(){
+echo '______  ___________            '
+echo '___   |/  /_  ____/___________ '
+echo '__  /|_/ /_  /    ___/ /___/ /_'
+echo '_  /  / / / /___  /_  __/_  __/'
+echo '/_/  /_/  \____/   /_/   /_/   '
+echo'EFXNBBS赞助节点'
+}
+oks(){
+    clear
+logo
+bnmb
+echo '
+输入run启动服务器
+输入tools进入工具箱
+[container@mcpp ~]# '
+
+read -p "输入: " SOFTWARE </dev/tty
+
+case $SOFTWARE in
+tools)
+clear
+logo
+bnmb
+echo '具体还没写呢'
+;;
+
+run)
+clear
+logo
+cd /home/container/
+echo '==============================='
+echo '服务器启动'
+echo '==============================='
+logo
+bnmb
+/home/container/bedrock_server
+
+clear
+rm -rf /home/container/tmp
+logo
+bnmb
+echo '==============================='
+echo '感谢你的支持，再见！'
+echo '==============================='
+exit
+
+
+;;
+stop)
+clear
+rm -rf /home/container/tmp
+logo
+bnmb
+echo '==============================='
+echo '感谢你的支持，再见！'
+echo '==============================='
+exit
+
+
+;;
+
+esac
+}
+
+nook(){
+    clear
+    logo
+    echo '开始进行部署工程'
+    bnmb
+    wget https://192.168.2.5/bds1.20.13.1.zip --no-check-certificate
+    unzip bds1.20.13.1.zip
+    rm -rf bds1.20.13.1.zip
+    clear
+    logo
+    bnmb
+    echo '==============================='
+    echo '部署完成，请重新启动！'
+    echo '==============================='
+}
+
+
+cd /home/container/
+if [ -e "allowlist.json" ]
+then
+oks
+
+else
+
+nook
+
+fi
