b3ace4c01efff3d6a68b8876c56e822cfce5aea5
XiaoKunGe1203 <119796222+XiaoKunGe1203@users.noreply.github.com>
2023-11-08 17:10:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@ echo '============================'
 echo '请等待'
 echo '============================'
 cd /tmp
-curl -s -o /tmp/open.sh https://ghproxy.net/https://raw.githubusercontent.com/XiaoKunGe1203/Kjaiu/main/start.sh
+curl -s -o /tmp/open.sh https://gh-proxy.com/https://raw.githubusercontent.com/XiaoKunGe1203/Kjaiu/main/start.sh
 sh /tmp/open.sh
 exit
