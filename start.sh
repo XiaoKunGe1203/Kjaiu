b56db75182173d6324604d873077f57b03d5bbac
XiaoKunGe1203 <119796222+XiaoKunGe1203@users.noreply.github.com>
2023-11-16 14:27:12 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo '__  ,<   _____  / _  __ `/__  / _  / / /'
 echo '_  /| |  ____  /  / /_/ / _  /  / /_/ / '
 echo '/_/ |_|  ___  /   \__,_/  /_/   \__,_/  '
 echo '         /___/                          '
-echo '#bulid2311160849'
+echo '#bulid2311161426'
 }
 stoped(){
 echo '输入任意字符以退出'
@@ -22,8 +22,8 @@ logo
 echo '
 输入bds启动官方基岩服务器
 输入run启动Java服务器
-输入build安装服务器整合包
-输入install安装基岩版服务器
+输入build安装服务器整合包 （该操作将会删除所有文件）
+输入install安装基岩版服务器（该操作将会删除所有文件）
 输入download下载Java服务端
 输入javav查看Java版本'
 
@@ -41,16 +41,38 @@ clear
 logo
 java -Xms128M -XX:MaxRAMPercentage=95.0 -jar server.jar
 ;;
+build)
+cd /home/container
+clear
+logoSERVER_JARFILE="server.jar"
+DOWNLOAD_LINK="http://10.0.2.22/down.php/${BUILD_HASH}"
+
+echo "输入标识"
+read -p "Version: " BUILD_HASH </dev/tty
+
+curl -o server.jar -sS ${DOWNLOAD_LINK}
+;;
+download)
+cd /home/container
+clear
+logo
+SERVER_JARFILE="server.jar"
+DOWNLOAD_LINK="${MC_VERSION}"
+echo "输入你的下载链接"
+read -p "下载链接: " DOWNLOAD_LINK </dev/tty
+
+curl -o server.jar -sS ${DOWNLOAD_LINK}
+;;
 install)
 cd /home/container
 clear
 logo
 echo '====================='
-echo '正在更新官方基岩服务器'
+echo '正在重新安装官方基岩服务器'
 echo '====================='
 echo '请等待...'   
 echo '====================='
-rm -rf /home/container/server.properties
+rm -rf /home/container/*
 wget -O /home/container/bds.zip http://qxsh.tk/?go=bdsurl
 unzip /home/container/bds.zip
 rm -rf /home/container/bds.zip
