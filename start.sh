b69e76c4d28e136142985811420e1b323317e16f
XiaoKunGe1203 <119796222+XiaoKunGe1203@users.noreply.github.com>
2023-11-16 10:55:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo '__  ,<   _____  / _  __ `/__  / _  / / /'
 echo '_  /| |  ____  /  / /_/ / _  /  / /_/ / '
 echo '/_/ |_|  ___  /   \__,_/  /_/   \__,_/  '
 echo '         /___/                          '
-echo '#bulid2311081849'
+echo '#bulid2311160849'
 }
 stoped(){
 echo '输入任意字符以退出'
@@ -21,7 +21,10 @@ clear
 logo
 echo '
 输入bds启动官方基岩服务器
-输入start启动Java服务器
+输入run启动Java服务器
+输入build安装服务器整合包
+输入install安装基岩版服务器
+输入download下载Java服务端
 输入javav查看Java版本'
 
 read -p "[container@kjaiu ~]# " SOFTWARE </dev/tty
@@ -32,7 +35,7 @@ clear
 logo
 java -version
 ;;
-start)
+run)
 cd /home/container
 clear
 logo
@@ -43,25 +46,7 @@ bds)
 cd /home/container
 clear
 logo
-if [ -e "bedrock_server" ]
-then
 ./bedrock_server
-else
-echo '====================='
-echo '正在更新官方基岩服务器'
-echo '====================='
-echo '请等待...'   
-echo '====================='
-rm -rf /home/container/server.properties
-wget -O /home/container/bds.zip http://qxsh.tk/?go=bdsurl
-unzip /home/container/bds.zip
-rm -rf /home/container/bds.zip
-clear
-logo
-echo '====================='
-echo '安装完成...'   
-echo '====================='
-fi
 ;;
 esac
 stoped
